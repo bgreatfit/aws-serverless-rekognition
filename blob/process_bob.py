eeffcb804e93b309d045d869b6e64eb3baad8f9b
Mike <mioshine2011@gmail.com>
2021-10-21 15:44:21 +0100

    bugfix for triggering s3 event

--- blob/process_bob.py
+++ blob/process_bob.py
@@ -1,7 +1,8 @@
 import http.client as httplib
 import os
-import requests
-from pynamodb.exceptions import DoesNotExist
+import boto3
+
+from pynamodb.exceptions import DoesNotExist, DeleteError, UpdateError
 from blob.asset_model import AssetModel
 
 
@@ -13,16 +14,33 @@ def event(event, context):
     try:
         if 'ObjectCreated:Put' == event_name:
 
-            blob = AssetModel.get(hash_key=blob_id)
-            result = blob.label_on_s3_upload(event)
-            if result:
-                blob.labels = result['image_labels']
-                blob.file_name = result['file_name']
-                blob.save()
-                if blob.callback_url != '':
-                    requests.post('http://httpbin.org/post', json={"key": "value"})
-            else:
-                requests.post('http://httpbin.org/post', json={"key": "value"})
+            try:
+                blob = AssetModel.get(hash_key=blob_id)
+                label_on_s3_upload(event, blob)
+                return {
+                    'statusCode': httplib.BAD_REQUEST,
+                    'body': {
+                        'error_message': 'Unable to update ASSET'}
+                }
+            except UpdateError:
+                return {
+                    'statusCode': httplib.BAD_REQUEST,
+                    'body': {
+                        'error_message': 'Unable to update ASSET'}
+                }
+
+        elif 'ObjectRemoved:Delete' == event_name:
+
+            try:
+                blob = AssetModel.get(hash_key=blob_id)
+                blob.delete()
+            except DeleteError:
+                return {
+                    'statusCode': httplib.BAD_REQUEST,
+                    'body': {
+                        'error_message': 'Unable to delete ASSET {}'.format(blob)
+                    }
+                }
 
     except DoesNotExist:
         return {
@@ -32,3 +50,26 @@ def event(event, context):
             }
         }
 
+    return {'statusCode': httplib.ACCEPTED}
+
+
+def label_on_s3_upload(event_obj, blob):
+    bucket = os.environ['S3_BUCKET']
+    region_name = os.environ['REGION']
+
+    files_uploaded = event_obj['Records']
+    image_labels = []
+    file_name = ''
+    for file in files_uploaded:
+        file_name = file["s3"]["object"]["key"]
+        rekognition_client = boto3.client('rekognition', region_name=region_name)
+        response = rekognition_client.detect_labels(Image={'S3Object': {'Bucket': bucket, 'Name': file_name}},
+                                                    MaxLabels=5)
+        for label in response['Labels']:
+            image_labels.append(label["Name"].lower())
+
+    # Add to DynamoDB
+
+    blob.labels = image_labels
+    blob.file_name = file_name
+    blob.save()
