54e537e66ac715ad3b4b92c96e88b427e0c04c8a
Mike <mioshine2011@gmail.com>
2021-10-21 10:55:27 +0100

    add create function to generate signed url

---
+++ blob/asset_model.py
@@ -0,0 +1,78 @@
+from datetime import datetime
+from enum import Enum
+import boto3
+import os
+from pynamodb.attributes import UnicodeAttribute, UTCDateTimeAttribute, ListAttribute
+from pynamodb.models import Model
+from log_cfg import logger
+from botocore.client import Config
+BUCKET = os.environ['S3_BUCKET']
+KEY_BASE = os.environ['S3_KEY_BASE']
+
+
+class State(Enum):
+    """
+    Manage asset states in dynamo with a string field
+    Could have used an int as well, or used a custom serializer which is a bit cleaner.
+    """
+    CREATED = 1
+    RECEIVED = 2
+    UPLOADED = 3
+    DELETED = 4
+
+
+class AssetModel(Model):
+    class Meta:
+        table_name = os.environ['DYNAMODB_TABLE']
+        if 'ENV' in os.environ:
+            host = 'http://localhost:8000'
+        else:
+            region = os.environ['REGION']
+            host = os.environ['DYNAMODB_HOST']
+
+    blob_id = UnicodeAttribute(hash_key=True)
+    state = UnicodeAttribute(null=False, default=State.CREATED.name)
+    callback_url = UnicodeAttribute(null=True)
+    labels = ListAttribute(null=True)
+    file_name = UnicodeAttribute(null=True)
+    createdAt = UTCDateTimeAttribute(null=False, default=datetime.now().astimezone())
+    updatedAt = UTCDateTimeAttribute(null=False, default=datetime.now().astimezone())
+
+    def __str__(self):
+        return 'blob_id:{}, state:{}'.format(self.blob_id, self.state)
+
+    def get_key(self):
+        return u'{}/{}'.format(KEY_BASE, self.blob_id)
+
+    def save(self, conditional_operator=None, **expected_values):
+        try:
+            self.updatedAt = datetime.now().astimezone()
+            logger.debug('saving: {}'.format(self))
+            super(AssetModel, self).save()
+        except Exception as e:
+            logger.error('save {} failed: {}'.format(self.asset_id, e), exc_info=True)
+            raise e
+
+    def __iter__(self):
+        for name, attr in self._get_attributes().items():
+            yield name, attr.serialize(getattr(self, name))
+
+    def get_upload_url(self, ttl=60):
+        """
+        :param ttl: url duration in seconds
+        :return: a temporary presigned PUT url
+        """
+        s3 = boto3.client('s3', config=Config(signature_version='s3v4'), region_name='us-west-2')
+
+        put_url = s3.generate_presigned_url(
+            ClientMethod='put_object',
+            Params={
+                'Bucket': BUCKET,
+                'Key': self.get_key(),
+            },
+            ExpiresIn=ttl,
+        )
+
+        logger.debug('upload URL: {}'.format(put_url))
+        return put_url
+
---
+++ blob/create.py
@@ -0,0 +1,34 @@
+import uuid
+import json
+import logging
+from blob.asset_model import AssetModel
+from log_cfg import logger
+
+
+def create(event, context):
+
+    data = json.loads(event['body']) if event['body'] else None
+    if data is not None and 'callback_url' not in data:
+        logging.error('Validation Failed')
+        return {'statusCode': 422,
+                'body': json.dumps({'error_message': 'Callback Url cannot be empty'})}
+
+    callback_url = data['callback_url'] if data is not None else ''
+
+    logger.debug('event: {}'.format(event))
+    asset = AssetModel()
+    asset.blob_id = uuid.uuid1().__str__()
+    asset.callback_url = callback_url
+    asset.save()
+    upload_url = asset.get_upload_url(60000)  # No timeout specified here, use member param default
+
+    return {
+        "statusCode": 201,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "upload_url": upload_url,
+            "id": asset.blob_id
+        })
+    }
